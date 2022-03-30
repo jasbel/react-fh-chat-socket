8d101476d87eab18f335530651e5cde58aa334af
jasbel <asbel.elmec@gmail.com>
2022-03-29 22:56:42 -0400

    socket completed first version

--- src/hooks/useSocket.jsx
+++ src/hooks/useSocket.jsx
@@ -1,27 +1,46 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import io from 'socket.io-client'
 
 export const useSocket = (serverPath) => {
-  const socket = useMemo(
-    () => io.connect(serverPath, { transports: ['websocket'] }),
-    [serverPath]
-  )
+  // const socket = useMemo( () => io.connect(serverPath, { transports: ['websocket'] }),[serverPath] )
+  const [socket, setSocket] = useState(null)
   const [online, setOnline] = useState(false)
 
+  const connectSocket = useCallback(() => {
+    const token = localStorage.getItem('token')
+
+    const socketTemp = io.connect(serverPath, {
+      transports: ['websocket'],
+      autoConnect: true,
+      forceNew: true, // limpia conexion y fuerza a crear nueva conexion
+      query: {
+        'x-token-socket': token,
+      },
+    })
+
+    setSocket(socketTemp)
+  }, [serverPath])
+
+  const disconnectSocket = () => {
+    socket?.disconnect()
+  }
+
   useEffect(() => {
-    setOnline(socket.connected)
+    setOnline(socket?.connected)
   }, [socket])
 
   useEffect(() => {
-    socket.on('connect', () => setOnline(true))
+    socket?.on('connect', () => setOnline(true))
   }, [socket])
 
   useEffect(() => {
-    socket.on('disconnect', () => setOnline(false))
+    socket?.on('disconnect', () => setOnline(false))
   }, [socket])
 
   return {
     socket,
     online,
+    connectSocket,
+    disconnectSocket,
   }
 }
--- src/states/socket/SocketContext.jsx
+++ src/states/socket/SocketContext.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { createContext } from 'react'
 import { urlFront } from '../../helpers/config'
 import { useSocket } from '../../hooks/useSocket'
+import AuthContext from '../auth/AuthContext'
 
 export const SocketContext = createContext()
 
 export const SocketProvider = ({ children }) => {
-  const { socket, online } = useSocket(urlFront)
+  const { socket, online, connectSocket, disconnectSocket } =
+    useSocket(urlFront)
+
+  const { auth } = useContext(AuthContext)
+
+  useEffect(() => {
+    if (auth.logged) connectSocket()
+  }, [auth, connectSocket])
+
+  useEffect(() => {
+    if (!auth.logged) disconnectSocket()
+  }, [auth, disconnectSocket])
 
   return (
     <SocketContext.Provider value={{ socket, online }}>
